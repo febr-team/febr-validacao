d0eb0c02d61077d00e2880b5debfb88bdc510dd3
Mikael Messias <mikaelmessias@outlook.com>
2019-04-24 17:07:17 -0300

    Add the class Log
    This class will be used to store and manage the information collected during validation
    whenever an inconsistency is found.
    
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

---
+++ classes/Log.js
@@ -0,0 +1,8 @@
+/**
+ * Armazena as informações de inconsistências da planilha sendo validada.
+ * @constructor
+ * @author Mikael Messias <mikaelmessias@alunos.utfpr.edu.br>
+ */
+function Log() {
+  this.records = [];
+}
