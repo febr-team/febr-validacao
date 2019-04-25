07232503039b5211209af6f5619ceb4f32addcf8
Mikael Messias <mikaelmessias@outlook.com>
2019-04-25 13:36:35 -0300

    Add the class Observacao
    This class will be used to represent the Observacao sheet data.
    
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

---
+++ classes/Observacao.js
@@ -0,0 +1,9 @@
+/**
+ * Representa os dados da planilha Observação.
+ * @constructor
+ * @author Mikael Messias <mikaelmessias@alunos.utfpr.edu.br>
+ */
+function Observacao() {
+  // Armazena índice, código, unidade e valores de cada coluna.
+  this.columns = [];
+}
