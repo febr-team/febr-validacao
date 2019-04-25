78973603baa326ccca2aac46c52eb693f515028e
Mikael Messias <mikaelmessias@outlook.com>
2019-04-25 14:28:49 -0300

    Add the addColumn method to Observacao class
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- classes/Observacao.js
+++ classes/Observacao.js
@@ -18,3 +18,17 @@ function Column(code, unit) {
   this.unit = unit;
   this.rows = [];
 }
+
+/**
+ * @description Adiciona uma nova coluna com o código e a unidade de medida especifidados.
+ * @param {string} code - O código identificador da coluna
+ * @param {string} unit - A unidade de medida da coluna
+ * @returns {number} O indíce do valor adicionado ou 0 caso os parâmetros estejam em formatos incorretos
+ */
+Observacao.prototype.addColumn = function(code, unit) {
+  if(typeof(code) === "string" && typeof(unit) == "string") {
+    this.columns.push(new Column(code,unit));
+    return this.columns.length - 1;
+  }
+  return 0;
+}
