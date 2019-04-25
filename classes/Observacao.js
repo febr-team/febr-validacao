87eaa3712cc1e39b30a8685c9c9020784a2c04b1
Mikael Messias <mikaelmessias@outlook.com>
2019-04-25 14:22:51 -0300

    Add the class Column to store data from Observacao
    The Column class will store the data, while the Observacao class will handle them.
    
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- classes/Observacao.js
+++ classes/Observacao.js
@@ -1,9 +1,20 @@
 /**
- * Representa os dados da planilha Observação.
+ * Representa a planilha Observação
  * @constructor
  * @author Mikael Messias <mikaelmessias@alunos.utfpr.edu.br>
  */
 function Observacao() {
-  // Armazena índice, código, unidade e valores de cada coluna.
+  // Armazena instâncias da classe Columns
   this.columns = [];
+}
+
+/**
+ * Representa os dados da planilha Observação
+ * @constructor
+ * @author Mikael Messias <mikaelmessias@alunos.utfpr.edu.br>
+ */
+function Column(code, unit) {
+  this.code = code;
+  this.unit = unit;
+  this.rows = [];
 }
