61179bb25698786bc0e6fdba0275037af620fc40
Mikael Messias <mikaelmessias@outlook.com>
2019-04-25 15:02:20 -0300

    Add the findColumn method to the Observacao class
    Also, add documentation for Column class parameteres
    
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- classes/Observacao.js
+++ classes/Observacao.js
@@ -1,5 +1,5 @@
 /**
- * Representa a planilha Observação
+ * Representa a planilha Observação.
  * @constructor
  * @author Mikael Messias <mikaelmessias@alunos.utfpr.edu.br>
  */
@@ -9,8 +9,10 @@ function Observacao() {
 }
 
 /**
- * Representa os dados da planilha Observação
+ * Representa os dados da planilha Observação.
  * @constructor
+ * @param {string} code - O código identificador da coluna
+ * @param {string} unit - A unidade de medida da coluna 
  * @author Mikael Messias <mikaelmessias@alunos.utfpr.edu.br>
  */
 function Column(code, unit) {
@@ -33,7 +35,7 @@ Observacao.prototype.output = function() {
  * @description Adiciona uma nova coluna com o código e a unidade de medida especificados.
  * @param {string} code - O código identificador da coluna
  * @param {string} unit - A unidade de medida da coluna
- * @returns {number} O indíce do valor adicionado ou 0 caso os parâmetros estejam em formatos incorretos
+ * @returns {number} O indíce do valor adicionado ou 0 caso os parâmetros estejam em formatos incorretos.
  */
 Observacao.prototype.addColumn = function(code, unit) {
   if(typeof(code) === "string" && typeof(unit) == "string") {
@@ -41,4 +43,21 @@ Observacao.prototype.addColumn = function(code, unit) {
     return this.columns.length - 1;
   }
   return 0;
+}
+
+/**
+ * @description Pesquisa uma coluna pelo código identificador.
+ * @param {string} code - O código identificador
+ * @returns {number} Um número inteiro positivo representado a posição da coluna, ou -1 caso não seja encontrada.
+ */
+Observacao.prototype.findColumn = function(code) {
+  var index = -1;
+
+  this.columns.forEach(function(item, item_index) {
+    if(item.code === code) {
+      index = item_index;
+    }
+  });
+
+  return index;
 }
